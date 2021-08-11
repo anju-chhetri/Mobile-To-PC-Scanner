41fd28dcf96e4844b56f6b043d463d073f793ad5
anju-chhetri <anju9chhetri@gmail.com>
2021-08-11 10:55:21 +0545

    Files added

---
+++ Scan_effect.py
@@ -0,0 +1,53 @@
+ 
+import cv2
+import numpy as np
+
+def map(x, in_min, in_max, out_min, out_max):
+    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
+
+def highPassFilter(img,kSize):
+    if not kSize%2:
+        kSize +=1
+    kernel = np.ones((kSize,kSize),np.float32)/(kSize*kSize)
+    filtered = cv2.filter2D(img,-1,kernel)
+    filtered = img.astype('float32') - filtered.astype('float32')
+    filtered = filtered + 127*np.ones(img.shape, np.uint8)
+    filtered = filtered.astype('uint8')
+    return filtered
+
+def blackPointSelect(img, blackPoint):
+    img = img.astype('int32')
+    img = map(img, blackPoint, 255, 0, 255)
+    _, img = cv2.threshold(img, 0, 255, cv2.THRESH_TOZERO)
+    img = img.astype('uint8')
+    return img
+
+def whitePointSelect(img,whitePoint):
+    _,img = cv2.threshold(img, whitePoint, 255, cv2.THRESH_TRUNC)
+    img = img.astype('int32')
+    img = map(img, 0, whitePoint, 0, 255)
+    img = img.astype('uint8')
+    return img
+
+def blackAndWhite(img):
+    lab = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
+    (l,a,b) = cv2.split(lab)
+    img = cv2.add( cv2.subtract(l,b), cv2.subtract(l,a) )
+    return img
+def scan_effect(img):
+    #img = cv.imread("/home/anju_chhetri/Desktop/PLS/scan_49.jpg")
+    blackPoint = 66
+    whitePoint = 127
+    image = highPassFilter(img,kSize = 51)
+    image_white = whitePointSelect(image, whitePoint)
+    img_black = blackPointSelect(image_white, blackPoint)
+#     image=blackPointSelect(img)
+#     white = whitePointSelect(image)
+#     img_black = blackAndWhite(white)
+    return img_black
+
+#img = cv2.imread("/home/anju_chhetri/Desktop/flowers1/flower_all/flower1.jpg")
+#image = scan_effect(img)
+#cv2.imshow("fd",image)
+#cv2.waitKey(0)
+#cv2.destroyAllWindows()
---
+++ Scanner.py
@@ -0,0 +1,138 @@
+ 
+import cv2
+from pathlib import Path
+from PIL import Image
+import os
+import numpy as np
+from Scan_effect import *
+#cap = cv2.VideoCapture('rtsp://admin:123456@192.168.1.103:8080/H264?ch=1&subtype=0')
+def basic():
+    url = 'http://192.168.1.103:8080/video'
+    path = str(Path.home())
+    path = path + "/Desktop/Mobile-to-PC-Scanner/"
+    folder_name = "PLS"
+    folder = os.path.join(path, folder_name)
+    pdf_folder = os.path.join(folder, "pdf")
+    file_exists = os.path.isdir(folder)
+    if(not file_exists):
+        os.mkdir(folder)
+    pdf_exists = os.path.isdir(pdf_folder)
+    if(not pdf_exists):
+        os.mkdir(pdf_folder)
+    image_list = []
+    vid = cv2.VideoCapture(url)
+    img_count = len(os.listdir(folder))
+    count_increase = img_count
+    while(True):
+        ret, frame = vid.read()
+        width = int(frame.shape[1]*(50/100))
+        height = int(frame.shape[0] * (60/100))
+
+        frame = cv2.resize(frame, (width,height),interpolation=cv2.INTER_AREA)
+        frame = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
+        for_display = frame.copy()
+        #cv2.drawContours(for_display, fr_con, index, (255, 0, 0), 5)
+        cv2.imshow("video",for_display)
+        key = cv2.waitKey(1)
+        if  key==113:
+            break
+        if key == 32:
+            image_name = folder + "/scan_" +str(count_increase) +  ".jpg"
+            count_increase +=1
+            cv2.imwrite(image_name,frame)
+            image_list.append(frame)
+    vid.release()
+    cv2.destroyAllWindows()
+
+    scan_image_list =[]
+
+    for image in image_list:
+        ima = process_image(image)
+        ima = Image.fromarray(ima)
+        scan_image_list.append(ima)
+
+    first_image = scan_image_list[0]
+    scan_image_list = scan_image_list[1:]
+    pdf = pdf_folder +"/test.pdf"
+    first_image.save(pdf, save_all = True, append_images = scan_image_list)
+
+
+import numpy as np
+import cv2
+# image = cv2.imread("/home/anju_chhetri/Desktop/scan_images/scan_image6.jpg")
+# image = cv2.resize(image, (700, 800))
+# returned_image = process_image(image)
+# cv2.imshow("win", returned_image)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+def return_one_contour(contour , image):
+    max_contour=0
+    j=0
+    coordinate = np.array([])
+    for i in contour:
+        area = cv2.contourArea(i)
+        perimeter = cv2.arcLength(i, True)
+        vertices = cv2.approxPolyDP(i,perimeter*0.02,True)
+        if (area>1000) and (len(vertices)==4):
+            max_contour = area
+            coordinate = vertices
+    if(len(coordinate) == 4):
+        scanned = futher(coordinate, image)
+    else:
+        scanned = scan_effect(image)
+        scanned = cv2.resize(scanned, (600,700))
+         # scanned = image
+        #_,scanned = cv2.threshold(im,127,255,cv2.THRESH_BINARY)
+
+    return scanned
+
+def futher(coordinate, image):
+    scanned = scan_effect(image)
+    points = coordinate.reshape((4,2))
+    points = reorder(points)
+    point1 = np.float32(points)
+    point2 = np.float32([[0,0],[600,0],[0,700],[600,700]])
+    matrix = cv2.getPerspectiveTransform(point1, point2)
+    wrap_image = cv2.warpPerspective(scanned,matrix,(600,700))
+    img = wrap_image.astype(np.uint8)
+    return  img
+
+# def scan_effect(img):
+#     dilated_img = cv2.dilate(img, np.ones((7, 7), np.uint8))
+#     bg_img = cv2.medianBlur(dilated_img, 15)
+#     diff_img = 255 - cv2.absdiff(img, bg_img)
+#     norm_img = diff_img.copy()
+#     cv2.normalize(diff_img, norm_img, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8UC1)
+#     _, thr_img = cv2.threshold(norm_img, 230, 0, cv2.THRESH_TRUNC)
+#     cv2.normalize(thr_img, thr_img, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8UC1)
+#     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+#     ret, th = cv2.threshold(img,0,  255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+#     denoised = cv2.fastNlMeansDenoising(th, 11, 31, 9)
+#     th = cv2.adaptiveThreshold(blur, 255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,5,3)
+#     return th
+
+
+def reorder(points):
+    points_copy = np.zeros((4,1,2), dtype = np.int32)
+    sum_p = points.sum(1)
+    points_copy[0] = points[np.argmin(sum_p)]
+    points_copy[3] = points[np.argmax(sum_p)]
+    diff = np.diff(points,axis = 1)
+    points_copy[1] = points[np.argmin(diff)]
+    points_copy[2] = points[np.argmax(diff)]
+    return points_copy
+
+def process_image(image):
+    im_gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+    kernel = np.ones((5,5),np.uint8)
+    img_dilate = cv2.dilate(im_gray, kernel, 5)
+    im_blur = cv2.GaussianBlur(img_dilate,(3,3),0)
+    blur= cv2.erode(im_blur,kernel,iterations =5)
+    im_canny = cv2.Canny(blur,100,255)
+    cont,h=cv2.findContours(im_canny.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+    j=0
+    scanned = return_one_contour(cont, image)
+    return scanned
+
+if __name__ == "__main__":
+    basic()
